b818bf6bd2a4d33777fff94e140978e8f273476a
fernanda <fernandajaviera64@gmail.com>
2021-08-09 19:37:50 -0400

    arreglos

---
+++ lorem_generator.rb
@@ -0,0 +1,6 @@
+n = ARGV[0].to_i
+lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus dui nisi, sollicitudin ut leo nec, mattis viverra leo. In a vehicula ipsum, quis euismod neque. Nam bibendum dolor blandit, finibus purus vel, consequat eros. Phasellus velit nisl, convallis euismod condimentum vel, vulputate nec orci. Aliquam lobortis a nisi in bibendum. Nam eu orci vulputate, accumsan ipsum id, semper felis. Aliquam suscipit, quam mollis feugiat mollis, massa erat mollis mauris, non ullamcorper enim eros viverra lectus. Sed pharetra id felis pretium facilisis. Phasellus eu diam arcu. Cras et dignissim nisl. Integer et massa ultrices dui porta consequat. Mauris non erat consequat, tincidunt nibh et, tristique sem."
+
+n.times do 
+    puts lorem
+end    
---
+++ solo_pares2.rb
@@ -0,0 +1,10 @@
+n = ARGV[0].to_i
+par = 2
+
+n.times do |i|
+    print par 
+    print " "
+    par = par + 2 
+end
+
+print "\n"
